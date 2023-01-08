1c9ac2bf8b028ec73053545d5e5905bd55ff80e7
Rafael <rflernesto@gmail.com>
2023-01-07 22:42:36 -0400

    chore: add confirmation hash table init

---
+++ local-database/create-confirmation-hash-table.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "..... setting up configuration......."
+aws configure set aws_access_key_id default_access_key --profile=default
+aws configure set aws_secret_access_key default_secret_key --profile=default
+aws configure set aws_session_token default_security_token --profile=default
+aws configure set region us-east-1 --profile=default
+
+echo "... CREATING TABLE ......."
+aws dynamodb create-table --endpoint-url $AWS_ENDPOINT_URL \
+    --table-name confirmation-hash \
+    --attribute-definitions AttributeName=email,AttributeType=S\
+    --key-schema AttributeName=hash,KeyType=HASH\
+    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
